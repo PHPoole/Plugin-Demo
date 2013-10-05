e4c06ba9af0f4e92dd753770cae3bd52838f6d5a
Arnaud Ligny <arnaud@ligny.org>
2013-10-06 01:20:39 +0200

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -10,11 +10,11 @@ Class Demo extends PHPoole_Plugin
     {
         $event  = $e->getName();
         $params = $e->getParams();
-        $this->debug($event, $params, 'IN');
+        $this->trace(self::DEBUG, $event, $params, 'IN');
         // demo
         echo "[DEMO] preInit event\n";
         //
-        $this->debug($event, $params, 'OUT');
+        $this->trace(self::DEBUG, $event, $params, 'OUT');
         return $params;
     }
 }
