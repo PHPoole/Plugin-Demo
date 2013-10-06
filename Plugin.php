79fc1b38cdc8d2b47a67f6d6b60109254f3a0263
Arnaud Ligny <arnaud@ligny.org>
2013-10-06 23:26:57 +0200

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * PHPoole plugin Demo
+ * PHPoole demo plugin
  */
 Class Demo extends PHPoole_Plugin
 {
@@ -11,8 +11,20 @@ Class Demo extends PHPoole_Plugin
         $event  = $e->getName();
         $params = $e->getParams();
         $this->trace(self::DEBUG, $event, $params, 'IN');
-        // demo
+        //
         echo "[DEMO] preInit event\n";
+        //
+        $this->trace(self::DEBUG, $event, $params, 'OUT');
+        return $params;
+    }
+
+    public function postInit($e)
+    {
+        $event  = $e->getName();
+        $params = $e->getParams();
+        $this->trace(self::DEBUG, $event, $params, 'IN');
+        //
+        echo "[DEMO] postInit event\n";
         //
         $this->trace(self::DEBUG, $event, $params, 'OUT');
         return $params;
